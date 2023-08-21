c7310e2a60b411aa095d2ae2ebb808f8515fa9d8
yohovani vargas <yohovanivargas@gmail.com>
2023-08-20 19:26:52 -0600

    add Table Name

--- src/components/DbForm.jsx
+++ src/components/DbForm.jsx
@@ -1,30 +1,31 @@
-
-import { createRoot } from 'react-dom/client';
+import { createRoot } from "react-dom/client";
 import React, { useContext, useState } from "react";
 import { DbContext } from "../context/DbContext";
 import ColumnForm from "./ColumnForm";
 
 export default function DbForm() {
-  const { setNameDB } = useContext(DbContext);
-  const [numCol, setNumCol] = useState(0)
-  const [table, setTable] = useState({})
-  const [numTable, setNumTable] = useState(-1)
+  const { setNameDB, tables } = useContext(DbContext);
+  const [numCol, setNumCol] = useState(0);
+  const [numTable, setNumTable] = useState(-1);
   const { addTable } = useContext(DbContext);
 
   const ChildComponent = () => {
-    return <ColumnForm numCol={numCol} numTable={numTable}/>;
+    return <ColumnForm numCol={numCol} numTable={numTable} />;
   };
-
 
   const [childComponents, setChildComponents] = useState([]);
 
   const addChildComponent = () => {
-    setChildComponents([...childComponents, <ChildComponent key={childComponents.length} />]);
+    setChildComponents([
+      ...childComponents,
+      <ChildComponent key={childComponents.length} />,
+    ]);
   };
 
   return (
     <div className="card mb-12">
-      <div>DB Definition</div><hr />
+      <div>DB Definition</div>
+      <hr />
       <div className="card-body">
         <div className="input-group mb-3">
           <span className="input-group-text" id="basic-addon1">
@@ -43,7 +44,16 @@ export default function DbForm() {
           <div className="card-header">Tables Definition</div>
           <div className="card-body">
             <div className="d-grid gap-1">
-              <button className="btn btn-primary" type="button" data-bs-toggle="modal" data-bs-target="#FormDbModal" onClick={() => {setNumTable(numTable+1); addTable({"name":"","columns":[]})}}>
+              <button
+                className="btn btn-primary"
+                type="button"
+                data-bs-toggle="modal"
+                data-bs-target="#FormDbModal"
+                onClick={() => {
+                  setNumTable(numTable + 1);
+                  addTable({ name: "", columns: [] });
+                }}
+              >
                 Add Table
               </button>
             </div>
@@ -51,15 +61,42 @@ export default function DbForm() {
         </div>
       </div>
 
-
-      <div className="modal fade modal-xl" id="FormDbModal" tabIndex="-1" aria-labelledby="FormDbModalLabel" aria-hidden="true">
+      <div
+        className="modal fade modal-xl"
+        id="FormDbModal"
+        tabIndex="-1"
+        aria-labelledby="FormDbModalLabel"
+        aria-hidden="true"
+      >
         <div className="modal-dialog">
           <div className="modal-content">
             <div className="modal-header">
-              <h1 className="modal-title fs-5" id="FormDbModalLabel">Add Table</h1>
-              <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+              <h1 className="modal-title fs-5" id="FormDbModalLabel">
+                Add Table
+              </h1>
+              <button
+                type="button"
+                className="btn-close"
+                data-bs-dismiss="modal"
+                aria-label="Close"
+              ></button>
             </div>
             <div className="modal-body">
+              <div className="mb-3">
+                <label htmlFor="tableName" className="form-label">
+                  Table Name
+                </label>
+                <input
+                  type="email"
+                  className="form-control"
+                  id="tableName"
+                  placeholder="Table Name"
+                  onChange={(e) => {
+                    tables[numTable].name = e.target.value;
+                  }}
+                />
+              </div>
+
               <table className="table ">
                 <thead>
                   <tr>
@@ -80,18 +117,33 @@ export default function DbForm() {
                     <tr key={index}>{child}</tr>
                   ))}
                 </tbody>
-
               </table>
-              <button type="button" className="btn btn-success" onClick={() => {setNumCol(numCol+1); addChildComponent()}}>Add Column</button>
+              <button
+                type="button"
+                className="btn btn-success"
+                onClick={() => {
+                  setNumCol(numCol + 1);
+                  addChildComponent();
+                }}
+              >
+                Add Column
+              </button>
             </div>
             <div className="modal-footer">
-              <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-              <button type="button" className="btn btn-primary">Save changes</button>
+              <button
+                type="button"
+                className="btn btn-secondary"
+                data-bs-dismiss="modal"
+              >
+                Close
+              </button>
+              <button type="button" className="btn btn-primary">
+                Save changes
+              </button>
             </div>
           </div>
         </div>
       </div>
-
     </div>
   );
 }
